de84edc8ddede5ba140811ea731f9436cec540c2
Romeu I. L. Pires <rilpires@gmail.com>
2019-12-08 22:49:50 -0300

    Paralelização com ACC implementada

--- paralelo_acc_inteligente.cpp
+++ paralelo_acc_inteligente.cpp
@@ -3,16 +3,13 @@
 
 typedef double FLOAT;
 
-#ifndef OPERATIONS_PER_LOOP
-    #define OPERATIONS_PER_LOOP 4
-#endif
-
 FLOAT* gera_transposta( size_t matrix_tam , FLOAT* A ){
     FLOAT initial_t = omp_get_wtime();
     FLOAT* ret = (FLOAT*)malloc( sizeof(FLOAT)*matrix_tam*matrix_tam );
+    size_t matrix_vetor_tam = matrix_tam*matrix_tam;
     int i , j;
 
-    #pragma acc data copyin(A[:matrix_tam*matrix_tam]) copyout(ret[:matrix_tam*matrix_tam])
+    #pragma acc data copyin(A[:matrix_vetor_tam]) copyout(ret[:matrix_vetor_tam])
     #pragma acc parallel loop collapse(2)
     for( j = 0 ; j < matrix_tam ; j++ ){
         for( i = 0 ; i < matrix_tam ; i++ ){
@@ -21,111 +18,37 @@ FLOAT* gera_transposta( size_t matrix_tam , FLOAT* A ){
     }
 
     FLOAT final_t = omp_get_wtime();
-    // printf("Duracao do gera_transposta: %.3fs\n" , final_t-initial_t);
     return ret;
 }
 
-FLOAT* mult( size_t matrix_tam , FLOAT* A , FLOAT* B , size_t block_tam ){
-    FLOAT* C = (FLOAT*)malloc( sizeof(FLOAT) * matrix_tam * matrix_tam );
-    int block_x,block_y,linha,coluna,k, num_blocos = matrix_tam/block_tam , lim1 , lim2 ;
-    FLOAT temp_sum[8],temp_A;
-    FLOAT *C_linha_pointer , *A_linha_pointer;
-    FLOAT* B_coluna_pointers[8];
-    FLOAT* BT = gera_transposta(matrix_tam,B);
+FLOAT* mult( size_t matrix_tam , FLOAT* A , FLOAT* B ){
+    FLOAT*  C = (FLOAT*)malloc( sizeof(FLOAT) * matrix_tam * matrix_tam );
+    int     linha,coluna,i;
+    FLOAT   temp_sum;
+    FLOAT*  BT = gera_transposta(matrix_tam,B);
+    size_t  matrix_vetor_tam = matrix_tam*matrix_tam;
 
-    #pragma acc data copyin(A[:matrix_tam*matrix_tam],B[:matrix_tam*matrix_tam]) copyout(C[:matrix_tam*matrix_tam])
-    #pragma acc parallel loop collapse(2)
+    #pragma acc data \
+        copyin(A[:matrix_vetor_tam],BT[:matrix_vetor_tam]) \
+        copyout(C[:matrix_vetor_tam])
     {
-        for( block_x = 0 ; block_x < num_blocos ; block_x++ )
-        for( block_y = 0 ; block_y < num_blocos ; block_y++ )
+        #pragma acc parallel loop independent tile(8,8)
+        for( linha = 0 ; linha < matrix_tam ; linha++ )
+        for( coluna = 0 ; coluna < matrix_tam ; coluna ++ )
         {
-            lim1 = (block_y+1)*block_tam;
-            for( linha = block_y*block_tam ; linha < lim1 ; linha++ ){
-                C_linha_pointer = C + linha*matrix_tam;
-                A_linha_pointer = A + linha*matrix_tam;
-                lim2 = (block_x+1)*block_tam;
-                for( coluna = block_x*block_tam ; coluna < lim2 ; coluna += OPERATIONS_PER_LOOP ){
-                    temp_sum[0] = 0;
-                    B_coluna_pointers[0] = BT + (coluna+0)*matrix_tam;
-                    #if OPERATIONS_PER_LOOP >= 2
-                    temp_sum[1] = 0;
-                    B_coluna_pointers[1] = BT + (coluna+1)*matrix_tam;
-                    #if OPERATIONS_PER_LOOP >= 4
-                    temp_sum[2] = 0;
-                    temp_sum[3] = 0;
-                    B_coluna_pointers[2] = BT + (coluna+2)*matrix_tam;
-                    B_coluna_pointers[3] = BT + (coluna+3)*matrix_tam;
-                    #if OPERATIONS_PER_LOOP == 8
-                    temp_sum[4] = 0;
-                    temp_sum[5] = 0;
-                    temp_sum[6] = 0;
-                    temp_sum[7] = 0;
-                    B_coluna_pointers[4] = BT + (coluna+4)*matrix_tam;
-                    B_coluna_pointers[5] = BT + (coluna+5)*matrix_tam;
-                    B_coluna_pointers[6] = BT + (coluna+6)*matrix_tam;
-                    B_coluna_pointers[7] = BT + (coluna+7)*matrix_tam;
-                    #endif
-                    #endif
-                    #endif
-                    for( k = 0 ; k < matrix_tam ; k ++ ){
-                        temp_A = A_linha_pointer[k];
-                        temp_sum[0] += temp_A * B_coluna_pointers[0][k] ;
-                        #if OPERATIONS_PER_LOOP >= 2
-                        temp_sum[1] += temp_A * B_coluna_pointers[1][k] ;
-                        #if OPERATIONS_PER_LOOP >= 4
-                        temp_sum[2] += temp_A * B_coluna_pointers[2][k] ;
-                        temp_sum[3] += temp_A * B_coluna_pointers[3][k] ;
-                        #if OPERATIONS_PER_LOOP == 8
-                        temp_sum[4] += temp_A * B_coluna_pointers[4][k] ;
-                        temp_sum[5] += temp_A * B_coluna_pointers[5][k] ;
-                        temp_sum[6] += temp_A * B_coluna_pointers[6][k] ;
-                        temp_sum[7] += temp_A * B_coluna_pointers[7][k] ;
-                        #endif
-                        #endif
-                        #endif
-                    }
-                    C_linha_pointer[coluna+0] = temp_sum[0];
-                    #if OPERATIONS_PER_LOOP >= 2
-                    C_linha_pointer[coluna+1] = temp_sum[1];
-                    #if OPERATIONS_PER_LOOP >= 4
-                    C_linha_pointer[coluna+2] = temp_sum[2];
-                    C_linha_pointer[coluna+3] = temp_sum[3];
-                    #if OPERATIONS_PER_LOOP == 8
-                    C_linha_pointer[coluna+4] = temp_sum[4];
-                    C_linha_pointer[coluna+5] = temp_sum[5];
-                    C_linha_pointer[coluna+6] = temp_sum[6];
-                    C_linha_pointer[coluna+7] = temp_sum[7];
-                    #endif
-                    #endif
-                    #endif
-                }
-            }
+            for( temp_sum = 0 , i = 0 ; i < matrix_tam ; i ++ )
+                temp_sum += A[i + linha*matrix_tam] * BT[i + coluna*matrix_tam];
+            C[coluna + linha*matrix_tam] = temp_sum;
         }
     }
-
     free( BT );
     return C;
 }
 
 int main( int argc , char** argv ){
 
-    if( (OPERATIONS_PER_LOOP != 1 ) 
-    && (OPERATIONS_PER_LOOP != 2 )
-    && (OPERATIONS_PER_LOOP != 4 )
-    &&( OPERATIONS_PER_LOOP != 8 ) ){
-        printf("OPERATIONS_PER_LOOP deve ser 1 , 2 , 4 , 8 ou 16");
-        return 2;
-    }
-
     int matrix_tam = 1024 , block_tam , num_threads = 1;
     if( argc >= 2 ) matrix_tam = atoi( argv[1] );
-    if( argc >= 3 ) block_tam = atoi( argv[2] ); else block_tam = matrix_tam;
-    if( argc >= 4 ) num_threads = atoi( argv[3] ); else num_threads = 1;
-    omp_set_num_threads(num_threads);
-    if( block_tam < OPERATIONS_PER_LOOP ){
-        printf("block_tam deve ser menor ou igual a OPERATIONS_PER_LOOP \n");
-        return 2;
-    }
 
     char nome_matriz_a[64] , nome_matriz_b[64] , nome_resultado[64];
     sprintf( nome_matriz_a , "A_%d_%d.txt" , matrix_tam , matrix_tam );
@@ -136,13 +59,17 @@ int main( int argc , char** argv ){
     FLOAT* B = le_matriz_vetor<FLOAT>( nome_matriz_b , matrix_tam );
 
     FLOAT initial_t = omp_get_wtime();
-    FLOAT* C = mult( matrix_tam , A , B , block_tam);
+    FLOAT* C = mult( matrix_tam , A , B );
     FLOAT final_t = omp_get_wtime();
 
-    printf("Duracao da multiplicacao(TAM=%d,block_tam=%d,OPERATIONS_PER_LOOP=%d,num_threads=%d): %.3fs\n" 
-        , matrix_tam , block_tam , OPERATIONS_PER_LOOP , num_threads , final_t - initial_t );
+    printf("[ACC] - Duracao da multiplicacao (TAM=%d): %.3fs\n" , matrix_tam , final_t - initial_t );
 
     if( matrix_tam <= 1024 )
         escreve_matriz_sequencial( nome_resultado , matrix_tam , C );
 
+    printf("Os 3 primeiros valores de A , B e C:\n" );
+    printf("A: %f %f %f \n" , A[0] , A[1] , A[2] );
+    printf("B: %f %f %f \n" , B[0] , B[1] , B[2] );
+    printf("C: %f %f %f \n" , C[0] , C[1] , C[2] );
+    
 }
