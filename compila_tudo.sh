f133370a2e4ddbde52cc48f2721cb89aadb3b081
Romeu I. L. Pires <rilpires@gmail.com>
2019-12-11 04:09:08 -0300

    (typo fix)

--- compila_tudo.sh
+++ compila_tudo.sh
@@ -8,7 +8,7 @@ echo "Compilando paralelo_omp_inteligente.cpp" &&
 pgc++ -mp paralelo_omp_inteligente.cpp -o paralelo_omp_inteligente &&
 echo "Compilando paralelo_omp_inteligente_strassen.cpp" &&
 pgc++ -mp paralelo_omp_inteligente_strassen.cpp -o paralelo_omp_inteligente_strassen &&
-echo "Compilando paralelo_acc_inteligente_strassen.cpp" &&
+echo "Compilando paralelo_acc_inteligente.cpp" &&
 pgc++ -acc -ta=nvidia paralelo_acc_inteligente.cpp -o paralelo_acc_inteligente &&
 echo "Compilando paralelo_acc_inteligente_strassen.cpp" &&
 pgc++ -acc -ta=nvidia paralelo_acc_inteligente_strassen.cpp -o paralelo_acc_inteligente_strassen &&
